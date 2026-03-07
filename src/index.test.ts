df9876e5112af15788e49c8b137f78bb29fd8399
Jonathan Gamble <jdgamble555@gmail.com>
2026-03-07 17:46:26 -0600

    workflow

---
+++ src/index.test.ts
@@ -0,0 +1,10 @@
+import { formatPrice } from "./index.js";
+import { test, expect } from "vitest";
+
+test("formatPrice", () => {
+    expect(formatPrice(1234.56)).toBe("$1,234.56");
+});
+
+test("formatPrice with small value", () => {
+    expect(formatPrice(5)).toBe("$5.00");
+});
